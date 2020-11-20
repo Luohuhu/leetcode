32260c07528332107344399a63e922d5a6b365bf
Luohuhu <51892662+Luohuhu@users.noreply.github.com>
2020-11-20 17:28:59 +0800

    Add files via upload

--- 78/Solution.java
+++ 78/Solution.java
@@ -2,41 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    List<String> result = new ArrayList<String>();
-    String smallResult = "";
-    public List<String> restoreIpAddresses(String s) {
-        if (s.length()>12){
-            return result;
-        }
-        int n = s.length();
-        helper(0,0,s,smallResult,n);
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    List<Integer> result1 = new ArrayList<Integer>();
+    public List<List<Integer>> subsets(int[] nums) {
+        helper(0, result1, nums.length, nums);
         return result;
     }
-    void helper(int cur,int num, String s, String smallResult, int n){
-        System.out.println(smallResult +" "+ num);
-        if (cur==n){
-            if (num == 4) {
-                smallResult = smallResult.substring(0,smallResult.length()-1);
-                result.add(new String(smallResult));
-            }
-            return;
+    void helper(int cur, List<Integer> result1, int n, int[] nums){
+        if(cur==n){
+            result.add(new ArrayList<Integer>(result1));
         }
-        if (cur<n&&s.charAt(cur)=='0'){
-            smallResult += "0.";
-            helper(cur+1,num+1,s,smallResult, n);
-        }else {
-            for (int i = 1; i <= 3 && cur + i <= n; i++) {
-                String sub = s.substring(cur, cur + i);
-                if (Integer.parseInt(sub) <= 255) {
-                    //System.out.println(sub+" "+cur+" "+(cur+i));
-                    helper(cur + i, num + 1, s, smallResult+sub + ".", n);
-                }else {
-                    return;
-                }
-            }
-        }
-    }
-    public static void main(String arg[]){
-        System.out.println(new Solution().restoreIpAddresses("0000"));
+        result1.add(nums[cur]);
+        helper(cur+1, result1, n, nums);
+        result1.remove(result1.size()-1);
+        helper(cur+1, result1, n, nums);
     }
 }
